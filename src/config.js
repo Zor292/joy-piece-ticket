829cf4fcaa25a13f700302ed85e8ea9ab4b6266c
8.c2 <firasbouslimi8@gmail.com>
2026-03-03 01:05:57 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  TICKET_CATEGORY_ID: '1478142994627559495',
+  LOG_CHANNEL_ID: '1478010530785529856',
+  IMAGE_URL: 'https://media.discordapp.net/attachments/1407735667181621290/1477918864435183860/CAC31DCA-3DC7-4D5C-80A0-3B3972978129.jpg?ex=69a72b73&is=69a5d9f3&hm=1bb22c52b9030281d6c600f08710ee096ca6a3cd5ce73fdd01287520e4e56860&=&format=webp&width=1386&height=779',
+  BANNER_URL: 'https://media.discordapp.net/attachments/1407735667181621290/1477918864732848188/70E09E1C-A630-484B-9F04-7A8B5A0DC2E9.jpg?ex=69a72b73&is=69a5d9f3&hm=1aa04ac2b8c54215d58eff75cd59c9598b6307aee84369ba55623729ac4f5819&=&format=webp&width=1401&height=88',
+  EMBED_COLOR: 0x7b2dff,
+  PREFIX: '!',
+
+  ROLES: {
+    SENIOR: '1313900532707885167',
+    SUPPORT: '1313877058891419658',
+    TECH_ADMIN: '1314577351660797993',
+    PURCHASE_ADMIN: '1313900532707885167',
+    COMPLAINT_ADMIN: '1314577351660797993',
+    COMPENSATION_ADMIN: '1367933550263009390',
+  },
+
+  TICKET_TYPES: {
+    tech: {
+      label: 'الدعم الفني',
+      emoji: '🔧',
+      adminRole: '1314577351660797993',
+      title: 'مرحبا بك في دعم الفني الخاص بجوي بيس',
+      supportRole: '1313877058891419658',
+    },
+    purchase: {
+      label: 'تكت شراء',
+      emoji: '🛒',
+      adminRole: '1313900532707885167',
+      title: 'مرحبا بك في التكت الخاص بالشراء في جوي بيس',
+      supportRole: '1313900532707885167',
+    },
+    complaint: {
+      label: 'تكت شكوى',
+      emoji: '📢',
+      adminRole: '1314577351660797993',
+      title: 'مرحبا بك في التكت الشكوى بجوي بيس',
+      supportRole: '1314577351660797993',
+    },
+    compensation: {
+      label: 'تكت تعويض',
+      emoji: '💰',
+      adminRole: '1367933550263009390',
+      title: 'مرحبا بك في التكت التعويض بجوي بيس',
+      supportRole: '1367933550263009390',
+    },
+  },
+
+  RULES_DESCRIPTION: `**قوانين التكت:**\n\nيرجى عدم ازعاج الطاقم الاداري بالمنشن\n\nفي حال عدم الرد على التكت في غضون 10 دقائق سيتم تقفيل التكت\n\nيرجى الالتزام بالاحترام المتبادل بين الادارة والاعضاء\n\n**بالنسبة للادارة**\n\nفي حال استلم اداري اخر التكت يمنع انك تدخل بالتكت مهما كان السبب`,
+};
